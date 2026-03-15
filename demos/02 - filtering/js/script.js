d1375f05f94c0cdc550b71f7c2dcf7f594f8b0f3
fhwang05 <feliciasthwang@gmail.com>
2026-03-15 17:52:25 -0400

    03/13 demos

---
+++ demos/02 - filtering/js/script.js
@@ -0,0 +1,10 @@
+$(".filters button").click(function () {
+    
+    f = $(this).data("year");
+    $(f).fadeToggle(500);
+    // get the year that is associated with the button
+    // find an element that has that year 
+    // hide that element
+
+
+})
